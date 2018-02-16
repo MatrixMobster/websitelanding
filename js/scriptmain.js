35541e1e60426b8bced15cf269c09ba4fc1919c8
Atmos <36503280+MatrixMobster@users.noreply.github.com>
2018-02-16 10:01:01 -0500

    Update scriptmain.js

--- js/scriptmain.js
+++ js/scriptmain.js
@@ -1,3 +1,24 @@
+$(function() { $('a[href*=#]:not([href=#])').click(function() {
+ if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+var target = $(this.hash);
+target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+if (target.length) {
+$('html,body').animate({
+crollTop: target.offset().top }, 1000);
+return false;
+      
+      
+              }
+      
+      
+            }
+      
+      
+          });
+      
+      
+});
+
 function getEventTarget(e) {
     e = e || window.event;
     return e.target || e.srcElement; 
